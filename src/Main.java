3ce1d927d01b61adace108a923fb5fa7c577b3aa
acidic_duncan <duncanr2022@headroyce.org>
2020-12-04 09:23:10 -0800

    Started UI menu

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = new MainWorkspace();
+        root.getStylesheets().add(DrawingWorkspace.class.getResource("style.css").toExternalForm());
+
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.requestFocus();
+        primaryStage.toFront();
+
+    }
 
     public static void main(String[] args) {
 	// write your code here
