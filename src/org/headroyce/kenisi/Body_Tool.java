18afe8261f9c2ceda847ab04f4911e9ac7f819ef
ottoR <ottor2022@headroyce.org>
2020-12-19 11:32:27 -0800

    Began body_tool class

--- src/org/headroyce/kenisi/Body_Tool.java
+++ src/org/headroyce/kenisi/Body_Tool.java
@@ -1,7 +1,25 @@
 package org.headroyce.kenisi;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.scene.canvas.Canvas;
+
+import java.time.Duration;
+import java.time.Instant;
+
 public class Body_Tool {
 
-    private LList<Body> bodies;
+    private Linkedlist<Body> bodies;
+    private boolean mouseDown, mouseMove;
+    // Selected elements of the line
+    private int[] selectedPoint, oldSelectedPoint;
+    private IntegerProperty radius;
 
+    // View
+    private Canvas view;
+
+    public void mouseClick () { }
+
+    public void mouseRelease (double[] center, long duration) {
+        bodies.add(new Body(duration/ 1000.0, center[0], center[1])); //determine radius based on duration
+    }
 }
