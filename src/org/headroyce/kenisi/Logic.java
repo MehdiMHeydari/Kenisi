981b48239c9faa030a23672673f894826e33298b
mehdih2022 <mehdi79h@gmail.com>
2020-12-29 15:04:33 -0800

    linked list updated and game runner added

--- src/org/headroyce/kenisi/Logic.java
+++ src/org/headroyce/kenisi/Logic.java
@@ -1,9 +1,45 @@
 package org.headroyce.kenisi;
 
+
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import java.util.*;
 import java.util.ArrayList;
+import java.util.Random;
 
-public class Logic {
+
+// the whole screen aka game area
+    public class Logic extends AnimationTimer {
+
+    private long time;
+
+    LinkedList<Body> planets = new LinkedList<Body>();
+
+    public void Logic() {
+
+    }
+
+    public void gravatationpull(Body A, Body B){
+
+    }
+
+    //gamerunner
+    public void handle (long timein) {
+
+        Body primaryplanet;
+        Body localplanet;
+        for (int i = 0; i < planets.size(); i++){
+            primaryplanet = planets.get(i);
+            for (int j = i+1; j < planets.size(); j++){
+                localplanet = planets.get(j);
+                gravatationpull(primaryplanet, localplanet);
+
+            }
+
+        }
 
 
+    }
 
 }
+
