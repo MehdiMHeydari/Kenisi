6df276a5d3d3ab552fe81ba4a7b4b1ff10b7baba
mehdih2022 <mehdi79h@gmail.com>
2020-12-30 19:09:42 -0800

    mehdi update

--- src/org/headroyce/kenisi/Body_Tool.java
+++ src/org/headroyce/kenisi/Body_Tool.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 
 public class Body_Tool {
 
-    public static final Linkedlist<Body> bodies = new Linkedlist<>();
+    public static final LinkedList<Body> bodies = new LinkedList<>();
     private boolean mouseDown;
     private final Canvas view;
     private double startX, startY; //the starting point of the mouse
