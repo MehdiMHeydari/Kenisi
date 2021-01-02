4e4a70d2ff4227f8e0607cf32f1bfbbc776c53fb
mehdih2022 <mehdi79h@gmail.com>
2021-01-01 21:27:54 -0800

    working on grav

--- src/org/headroyce/kenisi/Body.java
+++ src/org/headroyce/kenisi/Body.java
@@ -53,8 +53,12 @@ public class Body {
         double localX = localplanet.getX();
         double localY = localplanet.getY();
 
-        if (((localX - localplanet.radius <= primX + this.radius) || (localX + localplanet.radius >= primX - this.radius)))
-            return ((localY - localplanet.radius) <= (primY + this.radius)) || ((localY + localplanet.radius) >= (primY - this.radius));
+        if (((localX - localplanet.radius <= primX + this.radius) && (localX + localplanet.radius >= primX + this.radius))  || ((localX + localplanet.radius >= primX - this.radius) && (localX - localplanet.radius <= primX - this.radius))){
+
+            return ((localY - localplanet.radius <= primY + this.radius) && (localY + localplanet.radius >= primY + this.radius) ) || ((localY + localplanet.radius >= primY - this.radius) && (localY - localplanet.radius <= primY - this.radius));
+        }
+
+
         return false;
     }
 
